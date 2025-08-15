d91407326147ee74ca4a47b8921b34da434414d7
NestorCalderon777 <nestor.calderon.m@mail.pucv.cl>
2025-08-14 22:20:38 -0400

    Update tarea2.py

--- TWO BUTTONS/tarea2.py
+++ TWO BUTTONS/tarea2.py
@@ -1,26 +1,18 @@
 
 # Funcion que encuentra los minimos clicks para llegar de n --> m
 def min_clicks(n, m):
-    
-    # Contador de clicks (o c como dice en el reporte)
     c = 0
-
-    # Bucle que se mantiene si y solo si n es distinto de m 
-    while n != m :
-        # Caso 1: en este caso solo podemos restar hasta llegar al valor de m
-        if n >= m:
-            n -= 1
-
-        # Caso 2: por otro lado en este duplicar el numero es los mas factible pero sin superar el valor de m
-        elif n * 2 <= m :
+    while n < m:
+        if n * 2 <= m:
             n *= 2
-
-        # Caso 3: finalmente si ninguno de los dos casos anteriores son viables restaremos 1 para actualizar n
         else:
-            n-= 1
-            
-    # Actualizamos los clicks y se retornan 
-        c += 1
+            # Si n*2 > m, restamos de golpe hasta m
+            c += m - n
+            n = m
+        c += 1  # Contamos el paso actual
+    # Si n > m (caso muy raro), solo restamos
+    if n > m:
+        c += n - m
     return c
             
     
@@ -30,6 +22,7 @@ print(min_clicks(4, 6)) # Salida esperada 2
 print(min_clicks(10, 10)) # Salida esperada 0
 print(min_clicks(10000,1)) # Salida esperada 9999
 print(min_clicks(2,16)) # Salida esperada 3
+
 
 
 
